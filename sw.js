f711da261ba8a034549abf6aa36c7edf1c8be148
nurlan-bagirli <44071356+nurlan-bagirli@users.noreply.github.com>
2026-09-21 18:15:12 +0400

    Refresh PWA cache for improved OCR

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="py-2026-09-21-v2";
+const CACHE="py-2026-09-21-v3";
 const CORE=["./","./index.html","./manifest.webmanifest","./data.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
